50fc9fecf20236e74a30163307549763386344be
f5-nascimento <nascimento.conceito@gmail.com>
2024-12-05 06:09:33 -0300

    page consulta e consulta.js

---
+++ js/consulta.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+    var btnConsultar = document.getElementById("btnConsultar");
+
+    btnConsultar.addEventListener("click", function(e){
+        e.preventDefault();
+        var dados_obj = localStorage.getItem("Cadastro");
+        console.log(dados_obj);
+        var dados_str = JSON.parse(dados_obj);
+        console.log(dados_str);
+        document.getElementById("username").textContent = dados_str.username;
+        document.getElementById("password").textContent = dados_str.password;
+        document.getElementById("area").textContent = dados_str.area;
+    })
+
+});
